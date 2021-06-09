54525a824b5ef5768f7eb8d63e6822837e69556b
mashreqsol <47236439+mashreqsol@users.noreply.github.com>
2021-06-09 10:56:09 +0500

    whatsapp API

--- src/components/Whatsapp.js
+++ src/components/Whatsapp.js
@@ -2,7 +2,7 @@ import React from "react";
 import WhatsAppWidget from "react-whatsapp-widget";
 import "react-whatsapp-widget/dist/index.css";
 const Whatsapp = () => {
-  return <WhatsAppWidget phoneNumber="00923000736431" />;
+  return <WhatsAppWidget phoneNumber="+923000736431" />;
 };
 
 export default Whatsapp;
