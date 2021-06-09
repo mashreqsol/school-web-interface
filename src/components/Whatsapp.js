ca1ba2451997614c0db1e6f4629df9fc3bbc565a
mashreqsol <47236439+mashreqsol@users.noreply.github.com>
2021-06-09 11:02:50 +0500

    whatsapp component modified

--- src/components/Whatsapp.js
+++ src/components/Whatsapp.js
@@ -2,7 +2,13 @@ import React from "react";
 import WhatsAppWidget from "react-whatsapp-widget";
 import "react-whatsapp-widget/dist/index.css";
 const Whatsapp = () => {
-  return <WhatsAppWidget phoneNumber="+923000736431" />;
+  return (
+    <WhatsAppWidget
+      phoneNumber="+923000736431"
+      companyName="Mashreq School System"
+      message="-السلام عليكم ورحمة الله وبركاته! الله تعالی آپ سے راضی ھو!"
+    />
+  );
 };
 
 export default Whatsapp;
